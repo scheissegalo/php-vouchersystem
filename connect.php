36b47d2c31408a21c87a63e5e0e3af5f4d6f0d56
scheissegalo <bkarich@gmail.com>
2022-09-20 21:27:28 -0400

    Update connect.php
    clearance

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@ $password = '';
 
 //Other Settings
 $mainURL = 'http://localhost/php-vouchersystem/'; // Main URL for this script
-$baseDirectory = 'output/'; // Voucher Output Directory
+$baseDirectory = 'output/'; // Voucher Output Directory make sure is's there, if not create it and give writing rights.
 $ValidateURL = $mainURL.'validate.php'; //URL for validation
 $font_path = 'D:\\xamp2\\htdocs\\php-vouchersystem\\Nunito-Regular.ttf'; // Nunito Font Folder, absolute path!
 $voucher15min = 'whc_gc_15.jpg'; // 15 min version of Voucher Background
